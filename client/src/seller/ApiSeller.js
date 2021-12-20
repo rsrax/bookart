dc6ee5f067e1a523254e388abe5badc84bd80eff
Maruf <maruf.glsica@gmail.com>
2021-12-20 19:55:44 +0530

    Complaint Updated

--- client/src/seller/ApiSeller.js
+++ client/src/seller/ApiSeller.js
@@ -1,84 +1,82 @@
 // Create product
-export const createProduct = async(userId, token, product) => {
-    return fetch(`/api/product/create/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: product
-    })
+export const createProduct = async (userId, token, product) => {
+  return fetch(`/api/product/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: product
+  })
     .then(response => {
-        return response.json();
+      return response.json();
     })
     .catch(err => {
-        console.log(err);
-    })
-}
+      console.log(err);
+    });
+};
 
 // Get categories from backend
 export const getCategories = () => {
-    return fetch(`/api/categories`, {
-        method: "GET"
-    })
+  return fetch(`/api/categories`, {
+    method: "GET"
+  })
     .then(response => {
-        return response.json()
+      return response.json();
     })
     .catch(err => console.log(err));
-}
+};
 
 // Get all products
 export const getProducts = () => {
-    return fetch(`/api/products?limit=100`, {
-        method: "GET"
+  return fetch(`/api/products?limit=100`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 };
-
 
 // Get a single product
-export const getProduct = (productId) => {
-    return fetch(`/api/product/${productId}`, {
-        method: "GET"
+export const getProduct = productId => {
+  return fetch(`/api/product/${productId}`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 };
-
 
 // Update a product
 export const updateProduct = (productId, userId, token, productData) => {
-    return fetch(`/api/product/${productId}/${userId}`, {
-        method: "PUT",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: productData   // not stringify because we are using form data as image is also there
+  return fetch(`/api/product/${productId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: productData // not stringify because we are using form data as image is also there
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 };
 
 // Delete a product
 export const deleteProduct = (productId, userId, token) => {
-    return fetch(`/api/product/${productId}/${userId}`, {
-        method: "DELETE",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
+  return fetch(`/api/product/${productId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 };
--- client/src/user/apiUser.js
+++ client/src/user/apiUser.js
@@ -1,81 +1,80 @@
 // Read user  information from backend
 export const read = (userId, token) => {
-    return fetch(`/api/user/${userId}`, {
-        method: 'GET',
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
+  return fetch(`/api/user/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
     .then(response => {
-        return response.json();
+      return response.json();
     })
     .catch(err => {
-        console.log(err);
-    })
-} 
+      console.log(err);
+    });
+};
 
 // Update profile
 export const update = (userId, token, userData) => {
-    return fetch(`/api/user/${userId}`, {
-        method: "PUT",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(userData)
+  return fetch(`/api/user/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(userData)
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 };
 
 // Update user in local storage
 export const updateUser = (user, next) => {
-    if(typeof window !== 'undefined')
-    {
-        if(localStorage.getItem('jwt'))
-        {
-            let auth = JSON.parse(localStorage.getItem("jwt"));
-            auth.user = user;
-            localStorage.setItem('jwt', JSON.stringify(auth))
-            next()
-        }
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("jwt")) {
+      let auth = JSON.parse(localStorage.getItem("jwt"));
+      auth.user = user;
+      localStorage.setItem("jwt", JSON.stringify(auth));
+      next();
     }
-}
-//Create a complaint
-export const createComplaint = async(userId, token, values) => {
-    return fetch(`/api/complaint/create/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: values
-    })
+  }
+};
+
+export const createComplaint = async (userId, token, values) => {
+  return fetch(`/api/complaint/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(values)
+  })
     .then(response => {
-        return response.json();
+      return response.json();
     })
     .catch(err => {
-        console.log(err);
-    })
-}
+      console.log(err);
+    });
+};
 
 // Get purchase history
 export const getPurchaseHistory = (userId, token) => {
-    return fetch(`/api/orders/by/user/${userId}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
+  return fetch(`/api/orders/by/user/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 };
