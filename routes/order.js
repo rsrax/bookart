da157f7dde4657d3741f81e0e42779384521b7cf
Rinkal-Chomal <202012082@daiict.ac.in>
2021-12-20 14:12:29 +0530

    orders routes correction

--- routes/order.js
+++ routes/order.js
@@ -7,7 +7,7 @@ const {create, addOrderToUserHistory, listOrders, getStatusValues, UpdateOrderSt
 const {decreaseQuantity} = require("../controllers/product");
 
 router.post('/order/create/:userId', requireSignin, isAuth, addOrderToUserHistory, decreaseQuantity, create);
-router.get('/order/list/:userId', requireSignin, isAuth, isEmployee, listOrders);
+router.get('/order/list/:userId', requireSignin, isAuth, listOrders);
 router.get('/order/status-values/:userId', requireSignin, isAuth, isEmployee, getStatusValues);
 router.put('/order/:orderId/status/:userId', requireSignin, isAuth, isEmployee, UpdateOrderStatus);
 
